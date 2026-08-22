89255c843cc3378bf9669c421b0c8e4b6ce99413
Yuu <206304251+nekomario28@users.noreply.github.com>
2026-08-22 21:37:54 +0900

    spatial-core: export Visual Block IR adapter

--- packages/spatial-core/src/index.js
+++ packages/spatial-core/src/index.js
@@ -2,6 +2,7 @@ export { TAU, clamp, deterministicScatter, hashText, wrapAngle } from "./math.js
 export { normalizeWeightedEdges } from "./relations.js";
 export { normalizeSpatialEdge, normalizeSpatialGraph, normalizeSpatialNode } from "./model.js";
 export { adaptGalaxyGraph } from "./adapters/galaxy-graph.js";
+export { adaptVisualBlockIr } from "./adapters/visual-block-ir.js";
 export {
   DEFAULT_FORCE_SETTINGS,
   createForceNodes,
