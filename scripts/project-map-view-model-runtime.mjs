45dcd2cf32520e189b936a70e8e9c089d1eba79a
Yuu <206304251+nekomario28@users.noreply.github.com>
2026-08-26 18:33:49 +0900

    Expose Local Graph through browser view model

--- scripts/project-map-view-model-runtime.mjs
+++ scripts/project-map-view-model-runtime.mjs
@@ -1,6 +1,7 @@
 import { createProjectMapViewModelApi } from "../packages/project-map-view-model/src/index.js";
+import { createProjectMapLocalGraphApi } from "../packages/project-map-view-model/src/local-graph.js";
 import { normalizeWeightedEdges } from "../packages/spatial-core/src/relations.js";
 
 export function projectMapViewModelRuntimeSource() {
-  return `"use strict";\n/* global window */\n(() => {\n  const normalizeWeightedEdges = ${normalizeWeightedEdges.toString()};\n  window.ProjectMapViewModel = (${createProjectMapViewModelApi.toString()})({ normalizeWeightedEdges });\n})();\n`;
+  return `"use strict";\n/* global window */\n(() => {\n  const normalizeWeightedEdges = ${normalizeWeightedEdges.toString()};\n  const base = (${createProjectMapViewModelApi.toString()})({ normalizeWeightedEdges });\n  const localGraph = (${createProjectMapLocalGraphApi.toString()})({ projectByStatuses: base.projectByStatuses });\n  window.ProjectMapViewModel = Object.freeze({ ...base, projectLocalGraph: localGraph.project });\n})();\n`;
 }
